3881ae35de4e4dcbbe4ef503fcf81c955cecacee
Hotkang <353223003@qq.com>
2018-08-11 14:42:29 +0800

    First Commit
    respository initialize

---
+++ Assets/2DGamekit/Scripts/Hotkang/Demo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Gamekit2D
+{
+    public class Demo : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Debug.Log("On Collidision Endter");
+        }
+
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            Debug.Log("On Collidision Stay");
+        }
+    }
+}
